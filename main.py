3067753ed734f8f5fc5cf3da15499d2efb92fd04
Shikher Srivastava <shikher707@gmail.com>
2019-03-04 03:38:17 +0530

    Pre-Review 2 version final commit

--- main.py
+++ main.py
@@ -4,12 +4,10 @@ import cv2
 import numpy as np
 from math import sqrt
 import os
-# from model_loader import getLabel,Load_Model
-
 from training import getSign
 
 
-#Type of Road signs which present in our machine learing model
+#   Types of Road signs which are present in our machine learning model
 SIGNS = ["OTHER",
         "Horn Prohibited",
         "Stop",
@@ -24,65 +22,27 @@ SIGNS = ["OTHER",
         "OTHER"
         ]
 
-# Preprocessing the image for effecient detection
-def preprocess_image(image):
-    image = LaplacianOfGaussian(image)
-    image = binarization(image)
-    return image
 
 
-# LaplacianOfGaussian of image hels finding borders of the objects present in image
-def LaplacianOfGaussian(image):
-    LoG_image = cv2.medianBlur(image, 3)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    LoG_image = cv2.Laplacian(gray, cv2.CV_8U, 3, 3, 2)
-    LoG_image = cv2.convertScaleAbs(LoG_image)
-    #    cv2.imshow("LoG", LoG_image)
-    return LoG_image
-
-
-# Converting image to binary colors (Black/white) for effecient detecting
-def binarization(image):
-    thresh = cv2.threshold(image,22,255,cv2.THRESH_BINARY)[1]
-#    Adaptive Thresholding is more efficient but slow
-#    thresh = cv2.adaptiveThreshold(image,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
-#    cv2.imshow("Threshold",thresh)
-    return thresh
-
-# Clean the  previosly saved images
-def clean_images():
-	file_list = os.listdir('./')
-	for file_name in file_list:
-		if '.png' in file_name:
-			os.remove(file_name)
-
-
-# Removing small components which are of no importance
-def removeSmallComponents(image, threshold):
-
-    #Finding all your connected components in the image
-
+#   Removing small components which are of no importance
+def Small_Components_removal(image, threshold):
+#   Finding all your connected components in the image
     nb_components, output, stats, centroids = cv2.connectedComponentsWithStats(image, connectivity=8)
     sizes = stats[1:, -1]; nb_components = nb_components - 1
 
     img = np.zeros((output.shape),dtype = np.uint8)
 
-    #Defining acceptance threshold size of a connected component
+#   Defining acceptance threshold size of a connected component
 
     for i in range(0, nb_components):
         if sizes[i] >= threshold:
             img[output == i + 1] = 255
     return img
 
-#Finding contours of connected components of the image
-#Contour are array of pixel cordinates of all connected components respectively
-def findContour(image):
-    cnts = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = cnts[1]
-    return cnts
 
-#Checking if the contour has shape of sign (Ex. Circle,Hexagon,Triangle etc..)
-def contourIsSign(contour, centroid, threshold):
+
+#   Checking if the contour has shape of sign (Ex. Circle,Hexagon,Triangle etc..)
+def check_is_contour_Sign(contour, centroid, threshold):
     result=[]
     for cnt in contour:
         cnt = cnt[0]
@@ -93,25 +53,16 @@ def contourIsSign(contour, centroid, threshold):
     temp = sum(s for s in signature)
     temp = temp / len(signature)
 
-    # Condition for checking the shape
+#   Condition for checking the shape
 
     if temp > threshold or len(cv2.approxPolyDP(contour, 0.25* (cv2.arcLength(contour,  True)), True))==3:
         return True, max_value + 2
     else:
         return False, max_value + 2
 
-#Cropping Region of intreset in the image which contains the sign
-def cropSign(image, coordinate):
-    width = image.shape[1]
-    height = image.shape[0]
-    top = max([int(coordinate[0][1]), 0])
-    bottom = min([int(coordinate[1][1]), height-1])
-    left = max([int(coordinate[0][0]), 0])
-    right = min([int(coordinate[1][0]), width-1])
-    return image[top:bottom,left:right]
 
-# Finding the Largest sign among all the detected signs
-def findLargestSign(image, contours, threshold, distance_theshold):
+#   Finding the Greatest sign among all the detected signs
+def find_Greatest_Sign(image, contours, threshold, distance_theshold):
     max_distance = 0
     coordinate = None
     sign = None
@@ -121,34 +72,72 @@ def findLargestSign(image, contours, threshold, distance_theshold):
             continue
         cX = int(M["m10"] / M["m00"])
         cY = int(M["m01"] / M["m00"])
-        is_sign, distance = contourIsSign(c, [cX, cY], threshold)
+        is_sign, distance = check_is_contour_Sign(c, [cX, cY], threshold)
         if is_sign and distance > max_distance and distance > distance_theshold:
             max_distance = distance
             coordinate = np.reshape(c, [-1,2])
             left, top = np.amin(coordinate, axis=0)
             right, bottom = np.amax(coordinate, axis = 0)
             coordinate = [(left-2,top-2),(right+2,bottom+2)]
-            sign = cropSign(image,coordinate)
 
+            width = image.shape[1]
+            height = image.shape[0]
+            top = max([int(coordinate[0][1]), 0])
+            bottom = min([int(coordinate[1][1]), height - 1])
+            left = max([int(coordinate[0][0]), 0])
+            right = min([int(coordinate[1][0]), width - 1])
+            sign=image[top:bottom, left:right]
     return sign, coordinate
 
 
 def Locailizing_ROI(image, min_size_components, circle_detection_threshold, model, count, current_sign_type):
 
-
     original_image = image.copy()
-    image = cv2.bitwise_and(original_image, original_image, mask=remove_other_color(original_image))
+
+#   Red/Blue colored masking to remove unwanted areas from the image
+
+    image = cv2.bitwise_and(original_image, original_image, mask=red_blue_masking(original_image))
     cv2.imshow("Colored feature extraction",image)
-    binary_image = preprocess_image(image)
 
-    binary_image = removeSmallComponents(binary_image, min_size_components)
-#    cv2.imshow('BINARY', binary_image)
 
-    # binary_image = cv2.bitwise_and(binary_image,binary_image, mask=remove_other_color(image))
+#   Preprocessing the image for effecient detection Starts here..
+
+#   LaplacianOfGaussian of image helps finding borders of the objects present in image
+
+    LoG_image = cv2.medianBlur(image, 3)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    LoG_image = cv2.Laplacian(gray, cv2.CV_8U, 3, 3, 2)
+    LoG_image = cv2.convertScaleAbs(LoG_image)
+    #    cv2.imshow("LoG", LoG_image)
+    image= LoG_image
+
+#   Binarization
+#   Converting image into binary colors (Black/white) for effecient detecting
+
+    thresh = cv2.threshold(image, 22, 255, cv2.THRESH_BINARY)[1]
+#   Adaptive Thresholding is more efficient but slow
+    #thresh = cv2.adaptiveThreshold(image,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
+    #cv2.imshow("Threshold",thresh)
+    binary_image=thresh
+
+#   Preprocessing the image for effecient detection Ends here..
+
+#   Remove small sized components from the image which are of no intrest
+
+    binary_image = Small_Components_removal(binary_image, min_size_components)
+    #cv2.imshow('BINARY', binary_image)
+
+    # binary_image = cv2.bitwise_and(binary_image,binary_image, mask=red_blue_masking(image))
     cv2.imshow('BINARY IMAGE', binary_image)
 
-    contours = findContour(binary_image)
-    sign, coordinate = findLargestSign(original_image, contours, circle_detection_threshold, 25)
+#   Finding contours of connected components of the image
+#   Contour are array of pixel cordinates of all connected components respectively
+
+    cnts = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = cnts[1]
+
+#   Finding the Greatest sign among all the detected signs
+    sign, coordinate = find_Greatest_Sign(original_image, contours, circle_detection_threshold, 25)
     
     text = ""
     sign_type = -1
@@ -168,11 +157,11 @@ def Locailizing_ROI(image, min_size_components, circle_detection_threshold, mode
         cv2.putText(original_image,text,(coordinate[0][0], coordinate[0][1] -15), font, 1,(0,0,255),2,cv2.LINE_4)
     return coordinate, original_image, sign_type, text
 
-def remove_other_color(img):
+def red_blue_masking(img):
     frame = cv2.GaussianBlur(img, (3,3), 0) 
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    # define range of blue color in HSV
+#    define range of blue color in HSV
     lower_blue = np.array([101,50,28])
     upper_blue = np.array([130,255,255])
     mask_blue = cv2.inRange(hsv, lower_blue, upper_blue)
@@ -185,12 +174,12 @@ def remove_other_color(img):
     upper_red = np.array([10, 255, 255])
     mask0 = cv2.inRange(hsv, lower_red, upper_red)
 
-    # upper mask (170-180)
+#    upper mask (170-180)
     lower_red = np.array([170, 50, 50])
     upper_red = np.array([180, 255, 255])
     mask1 = cv2.inRange(hsv, lower_red, upper_red)
 
-    # join my masks
+#   join masks
     mask_red = mask0 + mask1
     # mask_red = cv2.inRange( hsv, lower_red, upper_red)
 
@@ -200,15 +189,22 @@ def remove_other_color(img):
     return mask_1
 
 def main():
-	#Clean previously saved images
-    clean_images()
+
+#   Clean previously saved images
+    file_list = os.listdir('./')
+    for file_name in file_list:
+        if '.png' in file_name:
+            os.remove(file_name)
+
     print('Loading Model...')
-    # model = Load_Model()
     with open('svm_model.pkl', 'rb') as file:
         model = pickle.load(file)
     print("Starting Detection...")
     vidcap = cv2.VideoCapture(0)
-#    'http://192.168.43.63:8080/video' for IP Webcam
+
+#    For IP Webcam 'http://192.168.43.63:8080/video'
+
+#   Codec For Saving Output Video
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
     out = cv2.VideoWriter('output.avi',fourcc, 20 , (640,480))
     circle_detection_threshold = 0.8   # parameter
@@ -229,7 +225,9 @@ def main():
             print("Sign: " + SIGNS[sign_type])
         cv2.imshow('Result', image)
         count = count + 1
-        # Write to video
+
+#        Write to video
+
         out.write(image)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
--- training.py
+++ training.py
@@ -2,11 +2,119 @@ import cv2
 from sklearn.metrics import classification_report,accuracy_score
 from sklearn.model_selection import train_test_split
 import pickle
+import time
 from sklearn import svm
 import numpy as np
 from os import listdir
 DIMENSION = 32
 NO_OF_CLASSES = 12
+
+
+
+
+def load_dataset():
+    signDataset = []
+    signLabels = []
+    for type in range(NO_OF_CLASSES):
+        list = listdir("./dataset/{}".format(type))
+        for file in list:
+            if '.png' in file:
+                path = "./dataset/{}/{}".format(type,file)
+                print(file)
+                img = cv2.imread(path,0)
+                img = cv2.resize(img, (DIMENSION, DIMENSION))
+                img = np.reshape(img, [DIMENSION, DIMENSION])
+                signDataset.append(img)
+                signLabels.append(type)
+                time.sleep(0.01)
+    return np.array(signDataset), np.array(signLabels)
+
+
+
+
+
+def get_HOGDescriptor() :
+    # Various Parameters used for converting image to HOG format
+    derivAperture = 1
+    winSigma = -1.
+    signedGradient = True
+    winSize = (20, 20)
+    blockSize = (10, 10)
+    blockStride = (5, 5)
+    histogramNormType = 0
+    L2HysThreshold = 0.2
+    gammaCorrection = 1
+    nlevels = 64
+    cellSize = (10,10)
+    nbins = 9
+    hog_descriptor = cv2.HOGDescriptor(winSize,blockSize,blockStride,cellSize,nbins,derivAperture,winSigma,histogramNormType,L2HysThreshold,gammaCorrection,nlevels, signedGradient)
+    return hog_descriptor
+
+
+
+if __name__ == '__main__':
+    print('Training data..')
+    time.sleep(1)
+    data, labels = load_dataset()
+    print(data.shape)
+
+    print('Getting HoG parameters ...')
+
+    hog = get_HOGDescriptor()
+
+    print('Computing HoG descriptors... ')
+    hog_descriptors = []
+    for img in data:
+        hog_descriptors.append(hog.compute(img))
+    hog_descriptors = np.squeeze(hog_descriptors)
+
+    # print('Randomizing and Spliting dataset into training(60%) and test data(40%).. ')
+    hog_descriptors_train, hog_descriptors_test, labels_train, labels_test = train_test_split(hog_descriptors, labels, test_size=0.4, random_state=4)
+    # train_n = int(0.99 * len(hog_descriptors))
+    # data_train, data_test = np.split(data, [train_n])
+    # hog_descriptors_train, hog_descriptors_test = np.split(hog_descriptors, [train_n])
+    # labels_train, labels_test = np.split(labels, [train_n])
+
+
+    model = svm.SVC(C=12.5, cache_size=200, class_weight=None, coef0=0.0, degree=3,
+        gamma=0.50625, kernel='rbf', probability=False )
+    # model.train(hog_descriptors_train, labels_train)
+    model.fit(hog_descriptors_train, labels_train)
+    print('SVM model training completed ...')
+    print('Testing model..')
+
+    response = model.predict(hog_descriptors_test)
+    # print(response)
+
+    # err = (labels_test != response).mean()
+    # print(err)
+    # print('Accuracy: %.2f %%' % ((1 - err) * 100))
+
+
+
+    print("Confusion Matrix:")
+    print(classification_report(labels_test, response))
+    print("Accuracy: " + str(accuracy_score(labels_test, response) * 100))
+    # print('Saving SVM model ...')
+    # with open('svm_model.pkl', 'wb') as file:
+    #     pickle.dump.0
+    #     2(model, file)
+
+
+
+# Used in main file for predicting sign using trained SVM model
+def getSign(model, data):
+    # Converting to recieved image to grayscale
+    gray = cv2.cvtColor(data, cv2.COLOR_BGR2GRAY)
+    # Converting Dimension of the image
+    img = [cv2.resize(gray,(DIMENSION,DIMENSION))]
+    # Calcualting HOG Descriptor for the image
+    hog = get_HOGDescriptor()
+    hog_descriptors = np.array([hog.compute(img[0])])
+    hog_descriptors = np.reshape(hog_descriptors, [-1, hog_descriptors.shape[1]])
+    # Predicting the class of the image or sign Type
+    pred = model.predict(hog_descriptors)
+    return int(pred)
 
 #
 # class Model(object):
@@ -34,105 +142,3 @@ NO_OF_CLASSES = 12
 #     def predict(self, samples):
 #
 #         return self.model.predict(samples)[1].ravel()
-
-
-
-def load_dataset():
-    signDataset = []
-    signLabels = []
-    for type in range(NO_OF_CLASSES):
-        list = listdir("./dataset/{}".format(type))
-        for file in list:
-            if '.png' in file:
-                path = "./dataset/{}/{}".format(type,file)
-                print(path)
-                img = cv2.imread(path,0)
-                img = cv2.resize(img, (DIMENSION, DIMENSION))
-                img = np.reshape(img, [DIMENSION, DIMENSION])
-                signDataset.append(img)
-                signLabels.append(type)
-    return np.array(signDataset), np.array(signLabels)
-
-
-
-
-
-def get_HOGDescriptor() :
-    # Various Parameters used for converting image to HOG format
-    derivAperture = 1
-    winSigma = -1.
-    signedGradient = True
-    winSize = (20, 20)
-    blockSize = (10, 10)
-    blockStride = (5, 5)
-    histogramNormType = 0
-    L2HysThreshold = 0.2
-    gammaCorrection = 1
-    nlevels = 64
-    cellSize = (10,10)
-    nbins = 9
-    hog_descriptor = cv2.HOGDescriptor(winSize,blockSize,blockStride,cellSize,nbins,derivAperture,winSigma,histogramNormType,L2HysThreshold,gammaCorrection,nlevels, signedGradient)
-    return hog_descriptor
-
-
-
-if __name__ == '__main__':
-    print('Loading road sign data..')
-    data, labels = load_dataset()
-    print(data.shape)
-
-    print('Getting HoG parameters ...')
-
-    hog = get_HOGDescriptor()
-
-    print('Computing HoG descriptors... ')
-    hog_descriptors = []
-    for img in data:
-        hog_descriptors.append(hog.compute(img))
-    hog_descriptors = np.squeeze(hog_descriptors)
-
-    print('Randomizing and Spliting dataset into training(60%) and test data(40%).. ')
-    hog_descriptors_train, hog_descriptors_test, labels_train, labels_test = train_test_split(hog_descriptors, labels, test_size=0.4, random_state=4)
-    # train_n = int(0.99 * len(hog_descriptors))
-    # data_train, data_test = np.split(data, [train_n])
-    # hog_descriptors_train, hog_descriptors_test = np.split(hog_descriptors, [train_n])
-    # labels_train, labels_test = np.split(labels, [train_n])
-
-    print('Training SVM model ...')
-    model = svm.SVC(C=12.5, cache_size=200, class_weight=None, coef0=0.0, degree=3,
-        gamma=0.50625, kernel='rbf', probability=False )
-    # model.train(hog_descriptors_train, labels_train)
-    model.fit(hog_descriptors_train, labels_train)
-    print('Testing model..')
-
-    response = model.predict(hog_descriptors_test)
-    # print(response)
-
-    # err = (labels_test != response).mean()
-    # print(err)
-    # print('Accuracy: %.2f %%' % ((1 - err) * 100))
-
-
-
-    print("Confusion Matrix:")
-    print(classification_report(labels_test, response))
-    print("Accuracy: " + str(accuracy_score(labels_test, response) * 100))
-    # print('Saving SVM model ...')
-    # with open('svm_model.pkl', 'wb') as file:
-    #     pickle.dump(model, file)
- 
-
-
-# Used in main file for predicting sign using trained SVM model
-def getSign(model, data):
-    # Converting to recieved image to grayscale
-    gray = cv2.cvtColor(data, cv2.COLOR_BGR2GRAY)
-    # Converting Dimension of the image
-    img = [cv2.resize(gray,(DIMENSION,DIMENSION))]
-    # Calcualting HOG Descriptor for the image
-    hog = get_HOGDescriptor()
-    hog_descriptors = np.array([hog.compute(img[0])])
-    hog_descriptors = np.reshape(hog_descriptors, [-1, hog_descriptors.shape[1]])
-    # Predicting the class of the image or sign Type
-    pred = model.predict(hog_descriptors)
-    return int(pred)
